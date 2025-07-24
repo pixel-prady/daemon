b06dad8f4b9eadad8349ce45620d09403620c1ef
pixel-prady <pradhumanchaudhary580a@gmail.com>
2025-07-24 12:40:29 +0530

    cpu monitor completed

---
+++ include/core/CPUIdleMonitor.hpp
@@ -0,0 +1,21 @@
+# ifndef CPU_IDLE_MONITOR_HPP
+# define CPU_IDLE_MONITOR_HPP
+
+# include <chrono>
+# include <thread>
+
+class CPUIdleMonitor{
+    public : 
+        explicit CPUIdleMonitor (double idleThreshold  = 15.0, int intervalMs = 1000); 
+
+        bool isCPUIdle () ; 
+
+    private : 
+        double IdleThereshold ; 
+        int IntervalMs ; 
+
+        std :: pair <unsigned long long  , unsigned long long > readCPUStats() ; 
+
+};
+
+# endif 
---
+++ src/core/CPUIdleMonitor.cpp
@@ -0,0 +1,55 @@
+#include "core/CPUIdleMonitor.hpp"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <utility>
+
+CPUIdleMonitor ::CPUIdleMonitor(double idleThreshold, int intervalMs)
+    : IdleThereshold(idleThreshold), IntervalMs(intervalMs) {}
+
+std ::pair<unsigned long long, unsigned long long> CPUIdleMonitor::readCPUStats()
+{
+    std ::ifstream statFile("/proc/stat");
+
+    if (!statFile.is_open())
+    {
+        throw std::runtime_error("Failed to open /proc/stat");
+    }
+
+    std ::string line;
+
+    std ::getline(statFile, line);
+    std ::istringstream ss(line);
+
+    std ::string cpu;
+
+    statFile.close();
+
+    unsigned long long user, nice, system, idle, iowait, irq, softirq, steal;
+
+    ss >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
+
+    unsigned long long idleTime = idle + iowait;
+    unsigned long long totalTime = user + nice + system + idle + iowait + irq + softirq + steal;
+
+    return {idleTime, totalTime};
+}
+
+bool CPUIdleMonitor::isCPUIdle()
+{
+    auto [idleTime1, totalTime1] = readCPUStats(); // taking first snap of cpu readings
+    std ::this_thread ::sleep_for(std ::chrono ::milliseconds(IntervalMs));
+
+    auto [idleTime2, totalTime2] = readCPUStats(); // taking second snap of cpu readings ;
+
+    unsigned long long idleTimeDiff = idleTime2 - idleTime1;
+    unsigned long long totalTimeDiff = totalTime2 - totalTime1;
+
+
+    if ( totalTimeDiff  == 0 ) return false;  
+
+    double cpuUsagePercentage = ( 1.0-(double) idleTimeDiff / totalTimeDiff) * 100 ; 
+
+    return cpuUsagePercentage < IdleThereshold; 
+}
