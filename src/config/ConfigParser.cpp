d2f310e8ab318977d84f242640e9eca12cd0dd64
pixel-prady <pradhumanchaudhary580a@gmail.com>
2025-07-25 23:28:42 +0530

    config header added

---
+++ src/config/ConfigParser.cpp
@@ -0,0 +1,27 @@
+#ifndef  CONFIG_PARSER_HPP
+#define  CONFIG_PARSER_HPP
+
+#include <string> 
+#include <unordered_map>
+
+class ConfigParser{
+    public : 
+        ConfigParser ( const std :: string & filepath); 
+
+        bool loadConfig();
+
+        std :: string getString ( const std :: string & key , const std :: string default= "") ; 
+        int getInt ( const std :: string & key , int default =0) ; 
+        bool getBool ( const std :: string & key , bool default = false) ;
+        double getDouble ( const std :: string & key , double default = 0.0) ;
+
+    private: 
+        std :: string filepath ; 
+        std :: unordered_map<std :: string > configData; 
+
+
+        bool parseYaml() ; 
+        bool parseJson() ;
+};  
+
+#endif; 
